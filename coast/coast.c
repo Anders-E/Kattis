227df70fe8d7d9a5356aa6c982ef82bfc21cd645
Anders Eriksson <aeri3@kth.se>
2017-02-02 02:22:49 +0100

    'Coast' now passes 1st test

---
+++ coast/coast.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int coast_search(char *map, char *visited, int n, int m, int i, int j, int len) {
+    int offset = i * m + j;
+
+    if (visited[offset])
+        return len;
+
+    if (map[offset] == '1') {
+        return len + 1;
+    }
+
+    visited[offset] = 1;
+
+    if (i > 0)     /* north */
+        len = coast_search(map, visited, n, m, i - 1, j, len);
+    if (i < n - 1) /* south */
+        len = coast_search(map, visited, n, m, i + 1, j, len);
+    if (j > 0)     /* west */
+        len = coast_search(map, visited, n, m, i, j - 1, len);
+    if (j < m - 1) /* east */
+        len = coast_search(map, visited, n, m, i, j + 1, len);
+
+    return len;
+}
+
+int coast_length(char *map, char *visited, int n, int m) {
+    int i, j, len = 0;
+
+    for (i = 0, j = 0; j < m - 1; j++) /* north side */
+        len = coast_search(map, visited, n, m, i, j, len);
+    for (; i < n - 1; i++) /* east side */
+        len = coast_search(map, visited, n, m, i, j, len);
+    for (; j > 0; j--) /* south side */
+        len = coast_search(map, visited, n, m, i, j, len);
+    for (; i > 1; i--) /* west side */
+        len = coast_search(map, visited, n, m, i, j, len);
+
+    return len;
+}
+
+int main() {
+    int n, m, i;
+    char *map, *visited;
+
+    scanf("%d %d", &n, &m);
+
+    map = malloc(n * m * sizeof(char));
+    visited = calloc(sizeof(char), n * m);
+
+    for (i = 0; i < n * m; i += m)
+        scanf("%s", &map[i]);
+
+    printf("%d\n", coast_length(map, visited, n, m));
+
+    free(visited);
+    free(map);
+
+    return 0;
+}
+
