f3a6e314116151b8cbf98167a1baea007d98ce5f
Anders Eriksson <aeri3@kth.se>
2017-04-04 23:22:55 +0200

    Solved 'Tarifa'

---
+++ tarifa/tarifa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, p, x;
+    int sum = 0;
+
+    scanf("%d %d", &x, &n);
+
+    while (n--) {
+        scanf("%d", &p);
+        sum += x - p;
+    }
+
+    printf("%d\n", sum + x);
+}
+
