4419dba95797225dc7e7b42769a8b31894942aae
Anders Eriksson <aeri3@kth.se>
2019-05-27 00:36:43 +0200

    Solve 'Catalan Numbers'

---
+++ catalan/catalan.hs
@@ -0,0 +1,21 @@
+main :: IO ()
+main = interact (writeOutput . solve . readInput)
+
+readInput :: String -> [Integer]
+readInput = (map read) . words
+
+solve :: [Integer] -> [Integer]
+solve nums = map cata (tail nums)
+
+writeOutput :: [Integer] -> String
+writeOutput = unlines . (map show)
+
+choose :: Integer -> Integer -> Integer
+choose n 0 = 1
+choose 0 k = 0
+choose n k = choose (n-1) (k-1) * n `div` k
+
+catas :: [Integer]
+catas = map cata [0..]
+cata :: Integer -> Integer
+cata n = (2 * n) `choose` n `div` (n + 1)
