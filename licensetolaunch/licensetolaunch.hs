47540860a0e12ef8773920b065a4f4c23d8cc77e
Anders Eriksson <aeri3@kth.se>
2019-08-01 15:50:43 +0200

    remove superfluous functions

--- licensetolaunch/licensetolaunch.hs
+++ licensetolaunch/licensetolaunch.hs
@@ -1,11 +1,8 @@
 main :: IO ()
-main = interact (writeOutput . solve . readInput)
+main = interact (show . solve . readInput)
 
 readInput :: String -> [Integer]
 readInput = (map read) . words
 
 solve :: [Integer] -> Integer
 solve nums = snd $ minimum $ zip (tail nums) [0..]
-
-writeOutput :: Integer -> String
-writeOutput = show
--- romans/romans.hs
+++ romans/romans.hs
@@ -1,11 +1,8 @@
 main :: IO ()
-main = interact (writeOutput . solve . readInput)
+main = interact (show . solve . readInput)
 
 readInput :: String -> Double
 readInput = read
 
 solve :: Double -> Integer
 solve miles = round $ miles * 1000 * 5280 / 4854
-
-writeOutput :: Integer -> String
-writeOutput = show
