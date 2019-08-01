7ddd3edd5a89170af0d93d19e694dfcd9ea084a9
Anders Eriksson <aeri3@kth.se>
2019-08-01 15:53:49 +0200

    remove superfluous function (again)

--- romans/romans.hs
+++ romans/romans.hs
@@ -1,8 +1,5 @@
 main :: IO ()
-main = interact (show . solve . readInput)
-
-readInput :: String -> Double
-readInput = read
+main = interact (show . solve . read)
 
 solve :: Double -> Integer
 solve miles = round $ miles * 1000 * 5280 / 4854
